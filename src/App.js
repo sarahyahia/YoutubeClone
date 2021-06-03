21ee50031bcb897a6a4de0212104af0fbe5a1934
Sarah <sarah.ali2810@gmail.com>
2021-06-03 03:21:15 +0200

    project setup and design header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./_app.scss"
+import {Container} from "react-bootstrap"
+import Header from './components/header/Header'
+import Sidebar from './components/sidebar/Sidebar';
+import HomeScreen from './screens/homeScreen/HomeScreen';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+      <div className="app__container">
+        <Sidebar/>
+        <Container fluid className="app__main border border-warining">
+          <HomeScreen/>
+        </Container>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./_categoriesBar.scss"
+
+const CategoriesBar = () => {
+    return (
+        <div>
+            Categories Bar
+        </div>
+    )
+}
+
+export default CategoriesBar
