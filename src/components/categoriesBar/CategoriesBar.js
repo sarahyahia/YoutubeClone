2df0d7a5d631c29b1560e0f3537ea5c6170cb213
Sarah <sarah.ali2810@gmail.com>
2021-08-12 02:20:01 +0200

    categories bar design

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./_categoriesBar.scss"
 
+const keywords = [
+    'All',
+    'React js',
+    'Angular js',
+    'React Native',
+    "use of API",
+    'Redux',
+    'Music',
+    'Art of Algorithms',
+    'Math',
+    'Movies',
+    'Django'
+]
+
+
 const CategoriesBar = () => {
+
+    const [activeElement, setActiveElement] = useState("All")
+    const handleClick = (value)=>{
+        setActiveElement(value)
+    }
+
     return (
-        <div>
-            Categories Bar
+        <div className="CategoriesBar">
+            {keywords.map((value,i)=> (
+                <span 
+                    onClick={()=> handleClick(value)}
+                    key={i}
+                    className={activeElement===value? "active":""}
+                >{value} 
+                </span>
+            ))}
         </div>
     )
 }
