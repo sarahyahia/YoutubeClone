f927b15de20d6f374a105593a2120321d9f4f5ea
Sarah <sarah.ali2810@gmail.com>
2021-08-30 07:42:41 +0200

    firebase setup and react routerdom

--- src/App.js
+++ src/App.js
@@ -6,26 +6,50 @@ import Sidebar from './components/sidebar/Sidebar';
 import HomeScreen from './screens/homeScreen/HomeScreen';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 
-function App() {
+const Layout = ({children}) => {
 
   const [sidebar, toggleSidebar] = useState(false)
   const handleToggleSidebar = () => toggleSidebar(value => !value)
 
+
   return (
-    // <>
-    //   <Header handleToggleSidebar = {handleToggleSidebar}/>
-    //   <div className="app__container">
-    //     <Sidebar 
-    //       sidebar={sidebar}
-    //       handleToggleSidebar = {handleToggleSidebar}
-    //     />
-    //     <Container fluid className="app__main border border-warining">
-    //       <HomeScreen/>
-    //     </Container>
-    //   </div>
-    // </>
-    <LoginScreen/>
+    <>
+      <Header handleToggleSidebar = {handleToggleSidebar}/>
+      <div className="app__container">
+        <Sidebar 
+          sidebar={sidebar}
+          handleToggleSidebar = {handleToggleSidebar}
+        />
+        <Container fluid className="app__main border border-warining">
+          {children}
+        </Container>
+      </div>
+    </>
+  )
+}
+
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Layout>
+            <HomeScreen/>
+          </Layout>
+        </Route>
+        <Route path="/auth">
+          <LoginScreen/>
+        </Route>
+        <Route path="/search">
+          <h1>Search</h1>
+        </Route>
+        <Redirect to="/"/>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNzfjtSEPvmd0xljQhh_QWKe2jHGE7Sn4",
+    authDomain: "clone-soyf.firebaseapp.com",
+    projectId: "clone-soyf",
+    storageBucket: "clone-soyf.appspot.com",
+    messagingSenderId: "1025511279800",
+    appId: "1:1025511279800:web:41ddb0f86d6424162b784d"
+  };
+
+firebase.initializeApp(firebaseConfig)
